ee10d39eecec45a72bfb4f88a32bdf54d37aa08c
SayedRZA <reza.hoss777@gmail.com>
2023-11-21 07:28:03 -0700

    added the mountain page

--- scripts/Filtersearch.js
+++ scripts/Filtersearch.js
@@ -15,39 +15,3 @@
     "Parkway"
   ];
 
-
-// const nationalParksArray = [
-    
-//   ];
-
-//   function filterByParkType() {
-//     const selectedParkType = document.getElementById('parkTypes').value;
-
-//     const filteredParks = nationalParksArray.filter(park =>
-//       selectedParkType ? parkTypesArray.includes(selectedParkType) : true
-//     );
-
-//     displayFilteredResults(filteredParks);
-//   }
-
-//   function displayFilteredResults(results) {
-//     const filteredResultsDiv = document.getElementById('filteredResults');
-//     filteredResultsDiv.innerHTML = '';
-
-//     if (results.length === 0) {
-//       filteredResultsDiv.innerHTML = 'No parks matching the selected type.';
-//       return;
-//     }
-
-//     const ul = document.createElement('ul');
-//     results.forEach(result => {
-//       const li = document.createElement('li');
-//       li.textContent = result.LocationName;
-//       ul.appendChild(li);
-//     });
-
-//     filteredResultsDiv.appendChild(ul);
-//   }
-
-
-
